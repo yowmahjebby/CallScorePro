c95922df4ba6c9159efa6575d8758f6b42f058b9
yowmahjebby <yowmahjebby@users.noreply.github.com>
2024-03-10 04:55:16 +0800

    Updated authorization and permission
    added decorators, pages, users, groups
    replaced promotional video in index

--- app/base/urls.py
+++ app/base/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
     path('logout/', views.logoutUser, name='logout'),
     path('home/', views.homePage, name='home'),
     path('profile/<str:username>', views.profilePage, name='profile'),
--- app/base/views.py
+++ app/base/views.py
@@ -1,29 +1,33 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib import messages 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
 from .forms import RegisterUserForm
+from .decorators import redirect_if_authenticated, allowed_users, superuser_only
 
 # Create your views here.
-def index(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    
+@redirect_if_authenticated
+def index(request):    
     if request.method == 'POST':
         if request.POST.get('submit') == 'Login':
             username = request.POST.get('username')
             password = request.POST.get('password')
 
             user = authenticate(request, username=username, password=password)
-
+            
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            
         elif request.POST.get('submit') == 'Register':
             form = RegisterUserForm(request.POST)
             if form.is_valid():
                 user = form.save(commit=False)
                 user.username = user.username.lower()
                 user.save()
+                group = Group.objects.get(name='Agent')
+                user.groups.add(group)
                 login(request, user)
                 return redirect('home')
             else:
@@ -31,16 +35,18 @@ def index(request):
 
     return render(request, 'base/index.html')
 
-def logoutUser(request):
-    logout(request)
-    return redirect('/')
-
-@login_required(login_url='login')
+@login_required(login_url='index')
 def homePage(request):
     return render(request, 'base/home.html')
 
-@login_required(login_url='login')
+@login_required(login_url='index')
 def profilePage(request, username):
     user = User.objects.get(username=username)
     context = {'user': user}
     return render(request, 'base/profile.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('/')
+
+# @allowed_users(allowed_roles=['admin'])
